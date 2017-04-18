49838dd6ee9cc97b795a2ae9c4b64a5ce8eb3888
flor <flor_gmr@hotmail.com>
2017-04-17 20:38:39 -0500

    add new changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	belongs_to :category, inverse_of: :products
+
+	valides :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418010253) do
+ActiveRecord::Schema.define(version: 20170418012943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20170418010253) do
     t.string   "name"
     t.integer  "price"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "premium",     default: false
   end
 
 end
