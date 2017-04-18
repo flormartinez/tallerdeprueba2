8ca82b77ff2c300ab5f6ab6f1aee4664eb8f664e
flor <flor_gmr@hotmail.com>
2017-04-17 20:59:13 -0500

    add seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
 	belongs_to :category, inverse_of: :products
 
-	valides :price, presence: true
+	validates :price, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Product.destroy_all
+
+category1 = Category.new(name: 'Category 1')
+category1.save
+
+category2 = Category.create name: 'Category 2'
+category3 = Category.create name: 'Category 3'
+
+Product.create name: 'Product 1', price: 100, category_id: category1.id
+Product.create name: 'Product 2', price: 150, category: category1
+category1.products << Product.create(name: 'Product 3', price: 100)
+
+
